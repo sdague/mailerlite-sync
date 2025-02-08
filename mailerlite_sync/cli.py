2e89ab0092ed357eb7d75a676e27fd6f96a12629
Sean Dague <sean@dague.net>
2025-02-08 10:28:53 -0500

    update all fields

--- mailerlite_sync/cli.py
+++ mailerlite_sync/cli.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import time
+import datetime
 
 import click
 import mailerlite as MailerLite
@@ -11,15 +12,32 @@ FIELD_MAP = {
     "last_name": "Last Name",
     "engagement_segment": "Engagement Segment",
     "congressional_district": "Congressional District",
+    "chapter_change_date": "Group Change Date",
+    "state_assembly_district": "State Assembly District",
+    "state_senate_district": "State District Upper House",
     "intro_call_date": "Date Of Intro Call",
+    "joined_ccl": "Date Added to Database",
+    "z_i_p": "Mailing Zip/Postal Code",
     "notes": "Notes/Comments",
     "state": "Mailing State/Province",
     "city": "Mailing City"
 }
 
+DATES = [
+    "Date Of Intro Call",
+    "Date Added to Database",
+    "Group Change Date"
+]
+
 NOT_FOUND = {'message': 'Resource not found.'}
 
 missing_emails = []
+
+def normalize_field(name, value):
+    if value and name in DATES:
+        return datetime.datetime.fromisoformat(value).date().strftime("%Y-%m-%d")
+    else:
+        return value
 
 def find_record(records, email):
     for rec in records:
@@ -54,7 +72,7 @@ def needs_update(ml_rec, row):
     fields = {}
     for k, v in FIELD_MAP.items():
         ml_field = ml_rec['fields'][k]
-        roster_field = row[v]
+        roster_field = normalize_field(v, row[v])
         if roster_field and ml_field != roster_field:
             click.echo(f'Updating {email} {k}: {ml_field} => {roster_field}')
             click.echo(ml_rec)
@@ -108,14 +126,11 @@ def first_command(roster, token):
                 fields = needs_update(response, row)
                 if fields:
                     response = client.subscribers.update(email, fields=fields)
-                    click.echo(response)
                 else:
                     click.echo(f"{email} unchanged")
     extras = extra_emails(records, roster_emails)
-    click.echo("Extra emails")
-    click.echo(extras)
-    click.echo('Missing emails')
-    click.echo(missing_emails)
+    click.echo("Extra emails (" + str(len(extras)) + "):\n\t" + "\n\t".join(sorted(extras)))
+    click.echo('Missing emails:\n\t' + "\n\t".join(sorted(missing_emails)))
     
     with open(roster) as f:
         ros = csv.DictReader(f)
